a3782da467b522fe7ab3b18ef61e9f40d17ecea2
takemura <takemura@UH20140808103NT>
2015-04-06 16:55:17 +0900

    DB設定変更

--- tests/_data/dump.sql
+++ tests/_data/dump.sql
@@ -1,1 +1,209 @@
-/* Replace this file with actual dump of your database */
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 2015 年 4 ?E06 日 09:54
+-- サーバのバージョン： 5.6.20
+-- PHP Version: 5.5.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `task_manager`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `company_masters`
+--
+
+CREATE TABLE IF NOT EXISTS `company_masters` (
+`id` int(10) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `delete_flag` char(1) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0'
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+
+--
+-- テーブルのデータのダンプ `company_masters`
+--
+
+INSERT INTO `company_masters` (`id`, `name`, `delete_flag`) VALUES
+(1, 'ユヒーロ', '0'),
+(2, 'NGC', '0'),
+(3, 'お茶ゼミ', '0');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `migrations`
+--
+
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `migrations`
+--
+
+INSERT INTO `migrations` (`migration`, `batch`) VALUES
+('2014_10_12_000000_create_users_table', 1),
+('2014_10_12_100000_create_password_resets_table', 1),
+('2015_03_30_003051_create_companyMasters_table', 1),
+('2015_03_30_033110_create_project_masters_table', 1),
+('2015_03_30_041314_create_tasks_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `password_resets`
+--
+
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `project_masters`
+--
+
+CREATE TABLE IF NOT EXISTS `project_masters` (
+`id` int(10) unsigned NOT NULL,
+  `company_id` int(10) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `delete_flag` char(1) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0'
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
+
+--
+-- テーブルのデータのダンプ `project_masters`
+--
+
+INSERT INTO `project_masters` (`id`, `company_id`, `name`, `delete_flag`) VALUES
+(1, 1, 'GA', '0'),
+(2, 2, 'CSC', '0'),
+(3, 2, 'navy', '0'),
+(4, 3, 'サイトリニューアル', '0');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `tasks`
+--
+
+CREATE TABLE IF NOT EXISTS `tasks` (
+`id` int(10) unsigned NOT NULL,
+  `project_id` int(10) unsigned NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `text` text COLLATE utf8_unicode_ci NOT NULL,
+  `limit` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `progress` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `delete` char(1) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+`id` int(10) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `company_masters`
+--
+ALTER TABLE `company_masters`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+ ADD KEY `password_resets_email_index` (`email`), ADD KEY `password_resets_token_index` (`token`);
+
+--
+-- Indexes for table `project_masters`
+--
+ALTER TABLE `project_masters`
+ ADD PRIMARY KEY (`id`), ADD KEY `project_masters_company_id_foreign` (`company_id`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+ ADD PRIMARY KEY (`id`), ADD KEY `tasks_project_id_foreign` (`project_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `company_masters`
+--
+ALTER TABLE `company_masters`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `project_masters`
+--
+ALTER TABLE `project_masters`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- ダンプしたテーブルの制約
+--
+
+--
+-- テーブルの制約 `project_masters`
+--
+ALTER TABLE `project_masters`
+ADD CONSTRAINT `project_masters_company_id_foreign` FOREIGN KEY (`company_id`) REFERENCES `company_masters` (`id`);
+
+--
+-- テーブルの制約 `tasks`
+--
+ALTER TABLE `tasks`
+ADD CONSTRAINT `tasks_project_id_foreign` FOREIGN KEY (`project_id`) REFERENCES `project_masters` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
