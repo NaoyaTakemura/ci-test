547318a365ee7fffbdf90fe310dd119f329f4bb1
takemura <takemura@UH20140808103NT>
2015-06-24 10:51:59 +0900

    タスク管理メニュー変更

--- resources/views/task/partial/menu.blade.php
+++ resources/views/task/partial/menu.blade.php
@@ -1,4 +1,5 @@
 					<div class="menu-area">
+						{!! HTML::linkRoute('tasks/index', 'タスク一覧', [], ['title'=>'タスク一覧', 'class' => 'btn btn-primary']) !!}
 						{!! HTML::linkRoute('tasks/createInput', '新規登録', [], ['title'=>'新規登録', 'class' => 'btn btn-primary']) !!}
 						{!! HTML::linkRoute('tasks/priorityList', 'プライオリティ変更', [], ['title'=>'プライオリティ変更', 'class' => 'btn btn-primary']) !!}
 						{!! HTML::linkRoute('tasks/calendar', 'カレンダー', [], ['title'=>'カレンダー', 'class' => 'btn btn-primary']) !!}
